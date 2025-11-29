574691d4b68df6baa8f4a278b4f8dbb3f60bce67
Damir <damir@test.com>
2025-11-29 17:57:59 +0500

    add login setup tests

--- playwright.config.js
+++ playwright.config.js
@@ -5,16 +5,15 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
   testDir: './tests',
-  snapshotPathTemplate: '{testDir}/snapshot/{testFilePath}/{arg}{ext}',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -37,19 +36,23 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: 'chromium authorized',
+      use: { ...devices['Desktop Chrome'], storageState: 'tests/playwright/.auth/user.json'},
+      dependencies: ['auth'],
+      testDir: 'tests/specs/authorized',
+      snapshotPathTemplate: 'tests/snapshot/authorized/{testFilePath}/{arg}{ext}',
+    },
+    {
+      name: 'chromium unauthorized',
       use: { ...devices['Desktop Chrome'] },
+      testDir: 'tests/specs/unathorized',
+      snapshotPathTemplate: 'tests/snapshot/unauthorized/{testFilePath}/{arg}{ext}',
+    },
+    {
+      name: 'auth',
+      testMatch: 'tests/specs/authTest.spec.js'
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
 
     /* Test against mobile viewports. */
     // {
